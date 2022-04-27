b666671576b1b9b3cd0e394cff35a94d249e5758
Dingyi Shen <dingyshe@amazon.com>
2022-04-27 12:09:58 -0700

    add uncovered code

---
+++ AmazonChimeSDK/AmazonChimeSDK/internal/utils/Foo.swift
@@ -0,0 +1,14 @@
+//
+//  Foo.swift
+//  AmazonChimeSDK
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+class Foo {
+    func foo()-> Bool {
+        return true
+    }
+}
